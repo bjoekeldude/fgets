3da500cf2190a854731d6c25c0a8b050bcbafec4
Stephan <sb@gruppe.ai>
2021-05-15 12:42:38 +0200

    implements the main-fct

---
+++ fgets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define INPUTBUFFSIZE 32
+
+int main (){
+	char Input_Buff[INPUTBUFFSIZE];
+
+	printf("Bitte Text eingeben: ");
+
+	if(fgets(Input_Buff, INPUTBUFFSIZE, stdin) ==
+	NULL){
+		printf("Fehlerhafter Input.");
+		abort();
+	}
+	else printf("Eingabe: %s.", Input_Buff);
+	//Warum steht der Punkt in der nächsten Zeile?
+	exit(0);
+}
